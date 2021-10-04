2e71bbf40ffe2e4da15895fa7c1c178fb2f76184
GuntKritamate <kritamate.agis@mail.kmutt.ac.th>
2021-10-04 23:55:26 +0700

    [FIX] try to detect press enter when searching

--- lib/screen/product_price_table_page.dart
+++ lib/screen/product_price_table_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pinto_admin_flutter/constant.dart';
 import 'package:pinto_admin_flutter/model/product_type.dart';
@@ -16,157 +17,168 @@ class _ProductPriceTableState extends State<ProductPriceTable> {
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: deepBlue,
-        title: Text(
-          'ตารางราคารับซื้อ',
-          style: kAppbarTextStyle,
+    // return RawKeyboardListener(
+    //   autofocus: true,
+    //   focusNode: FocusNode(),
+    //   onKey: (event){
+    //     if(event.isKeyPressed(LogicalKeyboardKey.backspace)){
+    //      setState(() {
+    //        print('Press enter');
+    //      });
+    //     }
+    //   },
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: deepBlue,
+          title: Text(
+            'ตารางราคารับซื้อ',
+            style: kAppbarTextStyle,
+          ),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back_ios),
+          ),
         ),
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back_ios),
-        ),
-      ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.fromLTRB(0.05 * screenWidth,
-                  0.03 * screenHeight, 0.05 * screenWidth, 0),
-              child: TextFormField(
-                textAlign: TextAlign.left,
-                style: kHeadingTextStyle,
-                decoration: const InputDecoration(
-                  hintText: 'ค้นหา',
-                  hintStyle: kNormalTextStyle,
-                  contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(0.05 * screenWidth,
+                    0.03 * screenHeight, 0.05 * screenWidth, 0),
+                child: TextFormField(
+                  textAlign: TextAlign.left,
+                  style: kHeadingTextStyle,
+                  decoration: const InputDecoration(
+                    hintText: 'ค้นหา',
+                    hintStyle: kNormalTextStyle,
+                    contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                      ),
                     ),
                   ),
-                  prefixIcon: Padding(
-                    padding: EdgeInsets.all(0.0),
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                    ),
-                  ),
+                  onChanged: (val) {
+                    setState(() {
+                      keyword = val;
+                      print(keyword);
+                    });
+                  },
                 ),
-                onChanged: (val) {
-                  setState(() {
-                    keyword = val;
-                    print(keyword);
-                  });
-                },
               ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(0.05 * screenWidth,
-                  0.025 * screenHeight, 0.05 * screenWidth, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'ตารางแสดงข้อมูล',
-                    style: kContentTextMedBlack,
-                  ),
-                  Text(
-                    'รายการสินค้ารับซื้อ',
-                    style: kHeadingBoldTextStyle,
-                  )
-                ],
-              ),
-            ),
-            Container(
-                //padding: EdgeInsets.all(10),
-                height: 0.65 * screenHeight,
-                color: mediumGrayBackground,
+              Container(
+                padding: EdgeInsets.fromLTRB(0.05 * screenWidth,
+                    0.025 * screenHeight, 0.05 * screenWidth, 0),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      color: mediumBlue,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          SizedBox(
-                            width: 0.15 * screenWidth,
-                            child: Text(
-                              'ลำดับ',
-                              style: kContentTextWhite,textAlign: TextAlign.center,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 0.35 * screenWidth,
-                            child: Text(
-                              'ชื่อสินค้า',
-                              style: kContentTextWhite,textAlign: TextAlign.left,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 0.15 * screenWidth,
-                            child: Text(
-                              'ซื้อ',
-                              style: kContentTextWhite,textAlign: TextAlign.right,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 0.15 * screenWidth,
-                            child: Text(
-                              'ขาย',
-                              style: kContentTextWhite,textAlign: TextAlign.right,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 0.2 * screenWidth,
-                            child: Text(
-                              'หน่วย',
-                              style: kContentTextWhite,textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ],
-                      ),
+                    Text(
+                      'ตารางแสดงข้อมูล',
+                      style: kContentTextMedBlack,
                     ),
-                    Expanded(
-                      child: FutureBuilder<List<ProductType>>(
-                        future: ProductService.getAllProductType(),
-                        builder: (BuildContext context,
-                            AsyncSnapshot<List<ProductType>> snapshot) {
-                          if (!snapshot.hasData) {
-                            return const Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          } else {
-                            //List<ProductType> productTypeList = snapshot.data!;
-                            List<ProductType> productTypeList = searchOperation(keyword, snapshot.data!);
-                            return ListView.builder(
-                                itemCount: productTypeList.length,
-                                itemBuilder: (context, index) =>
-                                    productPriceRow(
-                                        index: index + 1,
-                                        productName:
-                                            productTypeList[index].name,
-                                        buyPrice:
-                                            productTypeList[index].priceBuy,
-                                        sellPrice:
-                                            productTypeList[index].priceSell,
-                                        unit: productTypeList[index].unit));
-                          }
-                        },
-                      ),
-                    ),
+                    Text(
+                      'รายการสินค้ารับซื้อ',
+                      style: kHeadingBoldTextStyle,
+                    )
                   ],
-                )),
-          ],
+                ),
+              ),
+              Container(
+                  //padding: EdgeInsets.all(10),
+                  height: 0.65 * screenHeight,
+                  color: mediumGrayBackground,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        color: mediumBlue,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            SizedBox(
+                              width: 0.15 * screenWidth,
+                              child: Text(
+                                'ลำดับ',
+                                style: kContentTextWhite,textAlign: TextAlign.center,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 0.35 * screenWidth,
+                              child: Text(
+                                'ชื่อสินค้า',
+                                style: kContentTextWhite,textAlign: TextAlign.left,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 0.15 * screenWidth,
+                              child: Text(
+                                'ซื้อ',
+                                style: kContentTextWhite,textAlign: TextAlign.right,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 0.15 * screenWidth,
+                              child: Text(
+                                'ขาย',
+                                style: kContentTextWhite,textAlign: TextAlign.right,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 0.2 * screenWidth,
+                              child: Text(
+                                'หน่วย',
+                                style: kContentTextWhite,textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: FutureBuilder<List<ProductType>>(
+                          future: ProductService.getAllProductType(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<List<ProductType>> snapshot) {
+                            if (!snapshot.hasData) {
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            } else {
+                              //List<ProductType> productTypeList = snapshot.data!;
+                              List<ProductType> productTypeList = searchOperation(keyword, snapshot.data!);
+                              return ListView.builder(
+                                  itemCount: productTypeList.length,
+                                  itemBuilder: (context, index) =>
+                                      productPriceRow(
+                                          index: index + 1,
+                                          productName:
+                                              productTypeList[index].name,
+                                          buyPrice:
+                                              productTypeList[index].priceBuy,
+                                          sellPrice:
+                                              productTypeList[index].priceSell,
+                                          unit: productTypeList[index].unit));
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  )),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    //);
   }
   List<ProductType> searchOperation(String keyword, List<ProductType> listOfProduct) {
     List<ProductType> result = [];
