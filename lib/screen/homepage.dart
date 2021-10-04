e567528522b0629d982ed286e5c4afaeb9a62eda
Phuritat Chatyalak <chphuri@gmail.com>
2021-10-04 16:50:46 +0700

    feat: stock detail

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -32,124 +32,74 @@ class _HomepageState extends State<Homepage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'สวัสดี,\nแอดมิน สมหญิง',
-                    style: kHeadingTextStyle,
-                  ),
-                ],
-              ),
+            Text(
+              'สวัสดี,\nแอดมิน สมหญิง',
+              style: kHeadingTextStyle,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          textAlign: TextAlign.left,
-                          style: kHeadingTextStyle,
-                          decoration: const InputDecoration(
-                            hintText: 'ค้นหา',
-                            hintStyle: kNormalTextStyle,
-                            contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10),
-                              ),
-                            ),
-                            prefixIcon: Padding(
-                              padding: EdgeInsets.all(0.0),
-                              child: Icon(
-                                Icons.search,
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextFormField(
+                    textAlign: TextAlign.left,
+                    style: kHeadingTextStyle,
+                    decoration: const InputDecoration(
+                      hintText: 'ค้นหา',
+                      hintStyle: kNormalTextStyle,
+                      contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
                         ),
                       ),
-                    ],
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.all(0.0),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  width: screenWidth * 0.8,
-                  height: screenHeight * 0.63,
-                  child: Expanded(
-                      child: FutureBuilder<List<StockPreview>>(
-                    future: StockService.getStockPreviews(),
-                    builder: (BuildContext context,
-                        AsyncSnapshot<List<StockPreview>> snapshot) {
-                      if (!snapshot.hasData) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      } else {
-                        List<StockPreview> listStockPreview = snapshot.data!;
-                        return ListView.builder(
-                          itemCount: listStockPreview.length,
-                          itemBuilder: (context, index) =>
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                    // width: screenWidth * 0.8,
+                    // height: screenHeight * 0.70,
+                    child: FutureBuilder<List<StockPreview>>(
+                      future: StockService.getStockPreviews(),
+                      builder: (BuildContext context, AsyncSnapshot<List<StockPreview>> snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        } else {
+                          List<StockPreview> listStockPreview = snapshot.data!;
+                          return ListView.builder(
+                            itemCount: listStockPreview.length,
+                            itemBuilder: (context, index) =>
                               ProductCard.withoutProductID(
-                                  productName: listStockPreview[index].name,
-                                  inStock: listStockPreview[index]
-                                          .sellingAmount +
-                                      listStockPreview[index].preorderAmount,
-                                  unit: listStockPreview[index].Unit,
-                                  function: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                StockDashboard(
-                                                    productName:
-                                                        listStockPreview[index]
-                                                            .name)));
-                                  }),
-                        );
-                      }
-                    },
-                    //       builder: ListView(
-                    //   children: [
-                    //       ProductCard.withoutProductID(
-                    //           productName: 'ผักกาดขาว (mockData)',
-                    //           inStock: 10,
-                    //           unit: 'กรัม',
-                    //           function: () {
-                    //             Navigator.pushNamed(context, '/stock/stockDashboard');
-                    //           }),
-                    //       ProductCard.withoutProductID(
-                    //           productName: 'ผักกาดขาว',
-                    //           inStock: 10,
-                    //           unit: 'กรัม',
-                    //           function: () {}),
-                    //       ProductCard.withoutProductID(
-                    //           productName: 'ผักกาดขาว',
-                    //           inStock: 10,
-                    //           unit: 'กรัม',
-                    //           function: () {}),
-                    //       ProductCard.withoutProductID(
-                    //           productName: 'ผักกาดขาว',
-                    //           inStock: 10,
-                    //           unit: 'กรัม',
-                    //           function: () {}),
-                    //       ProductCard.withoutProductID(
-                    //           productName: 'ผักกาดขาว',
-                    //           inStock: 10,
-                    //           unit: 'กรัม',
-                    //           function: () {}),
-                    //
-                    //   ],
-                    // ),
-                  )),
-                )
-              ],
+                                productName: listStockPreview[index].name,
+                                inStock: listStockPreview[index]
+                                        .sellingAmount +
+                                    listStockPreview[index].preorderAmount,
+                                unit: listStockPreview[index].unit,
+                                function: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => StockDashboard(productName: listStockPreview[index].name))
+                                  );
+                                }
+                              ),
+                          );
+                        }
+                      },
+                    ),
+                  )
+                ],
+              ),
             ),
           ],
         ),
--- lib/service/stock_service.dart
+++ lib/service/stock_service.dart
@@ -1,4 +1,5 @@
 import 'package:pinto_admin_flutter/api/api.dart';
+import 'package:pinto_admin_flutter/model/stock.dart';
 import 'package:pinto_admin_flutter/model/stock_preview.dart';
 import 'package:dio/dio.dart';
 import 'package:pinto_admin_flutter/service/auth.dart';
@@ -14,6 +15,28 @@ class StockService {
       );
       List<StockPreview> stockPreview = (response.data as List).map((e) => StockPreview(e)).toList();
       return stockPreview;
+    } on DioError catch (err) {
+      return err.response!.data['message'];
+    }
+  }
+  static Future<Stock> getStockDetail(String productType) async{
+    try {
+      var response = await Api.dio.get('/stock-detail',
+        options: Options(
+          headers: {
+            'userId':Auth.user.userId
+          },
+        ),
+        queryParameters: {
+          'productType' : productType
+        },
+      );
+      Stock stockDetail = Stock(response.data);
+      return stockDetail;
+      // List<ProductType> stockdetail =  (response.data as List).map((productType) => ProductType(
+      //     productType
+      // )).toList();
+      // return stockdetail;
     } on DioError catch (err) {
       return err.response!.data['message'];
     }
